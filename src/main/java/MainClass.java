63f7a7cc1cef15e4628fab0733df217370c5138b
Kyle Plummer <kyle.plummer@revature.com>
2022-01-11 14:10:01 -0600

    this is the initial commit

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,9 @@
+//Note the PascalCase class name. this is the only place in Java where we capitalize the first letter of an
+//"identifier"
+public class MainClass {
+    public static void Main(String[] args) {
+        //program execution starts here.
+
+
+    }
+}
